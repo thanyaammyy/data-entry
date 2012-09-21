745c5bee111b9eec22b8b99dd9aeb7e8d65d9334
thanyaammyy@gmail.com <thanyaammyy@gmail.com@fc00252f-89f2-5e38-8f8c-bcdb38966390>
2012-09-21 01:54:10 +0000

    clear unused library

--- trunk/HotelDataEntry/HoltelDataEntryLib/Page/DataEntryHelper.cs
+++ trunk/HotelDataEntry/HoltelDataEntryLib/Page/DataEntryHelper.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Globalization;
 using System.Linq;
-using System.Text;
 
 namespace HotelDataEntryLib.Page
 {
